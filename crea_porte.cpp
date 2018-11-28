cb3f12ddfc73b2187d1a293cb92bc0a29faf950f
TrueAntoineLaroye <antoine.laroye@orange.fr>
2018-11-28 09:47:09 +0100

    Correction boucle infinie création porte.

--- crea_porte.cpp
+++ crea_porte.cpp
@@ -26,6 +26,7 @@ void crea_porte(char tab[MSIZE][MSIZE]){
   //on choisit deux cases pour créer la porte, en vérifiant qu'elles rentrent toutesl es deux dans la matrice.
   int m1, n1;
   while(crea_porte == false){
+    //On choisit la direction (haut ou bas) dans laquelle on place la porte.
     int direction = rand()%2;
     switch(direction){
       case 0:
@@ -40,10 +41,23 @@ void crea_porte(char tab[MSIZE][MSIZE]){
     cout << "Valeur de M1 : " << m1 << endl;
     cout << "Valeur de N1 : " << n1 << endl;
     if ((m1 > 0) && (m1 < MSIZE) && (n1 > 0) && (n1 < MSIZE)){
+      //Si on peut placer la porte, c'est bon.
       tab[m][n] = 'X';
       tab[m1][n1] = 'X';
       crea_porte = true;
       cout << "Porte créée." << endl;
+    }
+    else{
+      //Sinon, on change les valeurs de m et n pour modifier la position de la porte.
+      int m = rand()%8 + 1;
+      cout << "Valeur de M : " << m << endl;
+      int n = rand()%10;
+      cout << "Valeur de N : " << n << endl;
+      while(tab[m][n] != 'O'){
+        int m = rand()%8 + 1;
+        cout << "Valeur de M : " << m << endl;
+        int n = rand()%10;
+        cout << "Valeur de N : " << m << endl;
     }
   }
 }
