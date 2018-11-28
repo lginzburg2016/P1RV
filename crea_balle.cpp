98eec45c310466bf742f54ad3d6a1fd399c8ac9e
TrueAntoineLaroye <antoine.laroye@orange.fr>
2018-11-28 10:05:14 +0100

    correction probleme typage sur deplacements

--- crea_balle.cpp
+++ crea_balle.cpp
@@ -33,7 +33,7 @@ balle crea_balle(char tab[MSIZE][MSIZE]){
 
   }
   int new_dep;
-  deplacements = new vector<int>();
+  vector<int> deplacements = new vector<int>();
   for(int i=0; i<N_MOVES; i++){
     new_dep = rand()%9+1;
     deplacements.push_back(new_dep);
